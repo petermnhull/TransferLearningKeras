587dd30abdae864f4dcf4508ca93e3f88420ebe5
petermnhull <56369394+petermnhull@users.noreply.github.com>
2020-06-25 19:05:32 +0100

    Reorganisation, added dropout.

--- scripts/main.py
+++ scripts/main.py
@@ -1,46 +1,24 @@
 import tensorflow as tf
-from keras.layers import Dense, Input
-from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.models import load_model
 import os
 import pathlib
 import numpy as np
-from tqdm.keras import TqdmCallback
-from model import NewModel
 
 dir = os.path.dirname(__file__)
 
-BATCH_SIZE = 64
+def get_test_data():
+    X_test = np.load('test_set.npy') / 255
+    y_test = np.load('test_set_labels.npy') / 255
+    return X_test, y_test
 
-X_train = np.load('training_set.npy')
-y_train = np.load('training_set_labels.npy')
-X_test = np.load('test_set.npy')
-y_test = np.load('test_set_labels.npy')
-
-def get_trained_model():
-    model = NewModel()
-    model.build((1, 224, 224, 3))
-    model.freeze_weights()
-    model.summary()
-    model.compile(optimizer = 'Adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-    model.fit(
-        X_train,
-        y_train,
-        batch_size = BATCH_SIZE,
-        steps_per_epoch = BATCH_SIZE / len(X_train),
-        epochs = 20,
-        validation_split = 0,
-        verbose = 0,
-        callbacks = [TqdmCallback(verbose = 1)]
-        )
-    return model
-
-def test_model(model):
+def test_model(model, X_test, y_test):
     scores = model.evaluate(X_test, y_test)
     return scores
 
 def main():
-    model = get_trained_model()
-    scores = test_model(model)
+    model = load_model('trained_model')
+    X_test, y_test = get_test_data()
+    scores = test_model(model, X_test, y_test)
     return
 
 if __name__ == "__main__":
--- scripts/model.py
+++ scripts/model.py
@@ -1,24 +1,32 @@
 import tensorflow as tf
 from keras.models import Model
 from keras.applications import MobileNet
-from keras.layers import Dense, GlobalAveragePooling2D
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout
+import io
 
 
-class NewModel(tf.keras.Model):
+class MyModel(tf.keras.Model):
     def __init__(self):
-        super(NewModel, self).__init__()
+        super(MyModel, self).__init__()
         self.n_classes = 2
+        
         self.mobilenet = MobileNet(weights = 'imagenet', include_top = False)
         self.pool = GlobalAveragePooling2D()
         self.dense1 = Dense(1024, activation = 'relu')
         self.dense2 = Dense(512, activation = 'relu')
         self.out = Dense(self.n_classes, activation = 'softmax')
 
-    def call(self, inputs):
+        self.dropout = Dropout(0.5)
+
+    def call(self, inputs, training = None):
         x = self.mobilenet(inputs)
         x = self.pool(x)
         x = self.dense1(x)
+        if training:
+            x = self.dropout(x, training = training)
         x = self.dense1(x)
+        if training:
+            x = self.dropout(x, training = training)
         x = self.dense2(x)
         x = self.out(x)
         return x
@@ -30,3 +38,10 @@ class NewModel(tf.keras.Model):
         for layer in self.layers[1:]:
             layer.trainable = True
         return
+
+    def get_summary(self):
+        stream = io.StringIO()
+        self.summary(print_fn = lambda x: stream.write(x + '\n'))
+        summary_string = stream.getvalue()
+        stream.close()
+        return summary_string
