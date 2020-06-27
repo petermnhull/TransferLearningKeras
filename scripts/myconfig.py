da9ff8ad52d5bf53a83676fceeace8585cbe6e43
petermnhull <56369394+petermnhull@users.noreply.github.com>
2020-06-27 14:25:30 +0100

    Set up for face-shape data.

--- scripts/myconfig.py
+++ scripts/myconfig.py
@@ -5,6 +5,8 @@ CLASS_NAMES = ['Heart', 'Oblong', 'Oval', 'Round', 'Square']
 NUM_CLASSES = len(CLASS_NAMES)
 SAVE_HISTORY = True
 SAVE_MODEL = True
+SAVE_REPORT = True
+PRINT_REPORT = True
 
 # Paths
 PATH_MODEL = 'trained_model'
@@ -12,3 +14,4 @@ PATH_HISTORY = 'history.csv'
 PATH_TRAIN = 'training_set'
 PATH_TEST = 'test_set'
 PATH_DATA = 'data'
+PATH_TEST_REPORT = 'model_test.csv'
--- scripts/test.py
+++ scripts/test.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 from keras.models import load_model
 import numpy as np
+import pandas as pd
 from data import load_data
 from keras.utils import to_categorical
 from sklearn.metrics import classification_report
-from myconfig import BATCH_SIZE, PATH_MODEL, NUM_CLASSES, CLASS_NAMES
+from myconfig import BATCH_SIZE, PATH_MODEL, NUM_CLASSES, CLASS_NAMES, PATH_TEST_REPORT, PRINT_REPORT, SAVE_REPORT
 
 def test_model(model, X_test, y_test):
     y_pred = model.predict(
@@ -14,14 +15,18 @@ def test_model(model, X_test, y_test):
     )
     y_pred_classes = np.argmax(y_pred, axis = 1)
     print(y_test, '\n', y_pred_classes)
-    report = classification_report(y_test, y_pred_classes, target_names = CLASS_NAMES)
-    return report
+    report = classification_report(y_test, y_pred_classes, target_names = CLASS_NAMES, output_dict = True)
+    report_df = pd.DataFrame(report)
+    return report_df
 
 def main():
     model = load_model(PATH_MODEL)
     _, _, X_test, y_test = load_data()
-    report = test_model(model, X_test, y_test)
-    print(report)
+    report_df = test_model(model, X_test, y_test)
+    if PRINT_REPORT:
+        print(report_df)
+    if SAVE_REPORT:
+        report_df.to_csv(PATH_TEST_REPORT)
     return
 
 if __name__ == "__main__":
