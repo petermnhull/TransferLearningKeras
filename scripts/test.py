b97f5d55ad88d4c3d585ad3a86fd095ce8610fa7
petermnhull <56369394+petermnhull@users.noreply.github.com>
2020-06-27 14:02:32 +0100

    Added classification report.

--- scripts/test.py
+++ scripts/test.py
@@ -3,21 +3,25 @@ from keras.models import load_model
 import numpy as np
 from data import load_data
 from keras.utils import to_categorical
-from myconfig import BATCH_SIZE, PATH_MODEL, NUM_CLASSES
+from sklearn.metrics import classification_report
+from myconfig import BATCH_SIZE, PATH_MODEL, NUM_CLASSES, CLASS_NAMES
 
 def test_model(model, X_test, y_test):
-    scores = model.evaluate(
+    y_pred = model.predict(
         X_test,
-        to_categorical(y_test, NUM_CLASSES),
-        batch_size = BATCH_SIZE
-        )
-    return scores
+        batch_size = BATCH_SIZE,
+        verbose = 1
+    )
+    y_pred_classes = np.argmax(y_pred, axis = 1)
+    print(y_test, '\n', y_pred_classes)
+    report = classification_report(y_test, y_pred_classes, target_names = CLASS_NAMES)
+    return report
 
 def main():
     model = load_model(PATH_MODEL)
     _, _, X_test, y_test = load_data()
-    scores = test_model(model, X_test, y_test)
-    print(scores)
+    report = test_model(model, X_test, y_test)
+    print(report)
     return
 
 if __name__ == "__main__":
