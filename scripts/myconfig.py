362d42add448097bc659f4b5e49ee1a54522f140
petermnhull <56369394+petermnhull@users.noreply.github.com>
2020-06-27 18:29:53 +0100

    Training and testing complete.
    Training and testing complete for model with shuffled data.

---
+++ scripts/myconfig.py
@@ -0,0 +1,18 @@
+# Global Variables
+BATCH_SIZE = 64
+IMAGE_SHAPE = (224, 224, 3)
+CLASS_NAMES = ['Heart', 'Oblong', 'Oval', 'Round', 'Square']
+NUM_CLASSES = len(CLASS_NAMES)
+SAVE_HISTORY = True
+SAVE_MODEL = True
+SAVE_REPORT = True
+PRINT_REPORT = True
+SHUFFLE = True
+
+# Paths
+PATH_MODEL = 'trained_model'
+PATH_HISTORY = 'history.csv'
+PATH_TRAIN = 'training_set'
+PATH_TEST = 'test_set'
+PATH_DATA = 'data'
+PATH_TEST_REPORT = 'test_results.csv'
